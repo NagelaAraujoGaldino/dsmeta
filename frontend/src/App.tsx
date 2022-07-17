651e1bdc8f96f28467c30417950b37f8e8c6ca31
NagelaAraujoGaldino <nagelaaraujo224@gmail.com>
2022-07-16 21:52:07 -0300

    Projeto Criado

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import NotificacaoBotao from "./componentes/notificacaobotao"
+
+function App() {
+  return (
+    <>
+      <h1>Olá Mundo !</h1>
+      <NotificacaoBotao />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/componentes/notificacaobotao/index.tsx
@@ -0,0 +1,14 @@
+
+function NotificacaoBotao() {
+    return (
+
+
+        <h1>
+            <p>Meu Componente</p>
+
+        </h1>
+
+    )
+}
+
+export default NotificacaoBotao
